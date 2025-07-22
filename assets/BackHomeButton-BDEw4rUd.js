36629c36b00733fa33279368dbfe535c2882f6c8
Tomilov-G <tomilovgleb591@gmail.com>
2025-07-22 17:35:04 +0700

    Updates

--- assets/BackHomeButton-BDEw4rUd.js
+++
@@ -1,2 +0,0 @@
-import{j as t,L as s}from"./index-D_ikmzhg.js";const c="_link_1x7co_1",e="_button_1x7co_4",n={link:c,button:e},i=({title:o})=>t.jsx(s,{to:"/",className:n.link,children:t.jsx("button",{type:"button",className:n.button,children:o})});export{i as B};
-//# sourceMappingURL=BackHomeButton-BDEw4rUd.js.map
---
+++ assets/BackHomeButton-BSLivnq-.js
@@ -0,0 +1,2 @@
+import{j as t,L as s}from"./index-X6tlylXT.js";const c="_link_1x7co_1",e="_button_1x7co_4",n={link:c,button:e},i=({title:o})=>t.jsx(s,{to:"/",className:n.link,children:t.jsx("button",{type:"button",className:n.button,children:o})});export{i as B};
+//# sourceMappingURL=BackHomeButton-BSLivnq-.js.map
