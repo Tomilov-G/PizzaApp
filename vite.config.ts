5ce6a76cdd3d8fb1f759c4aa7f8f0c4296e95ad0
Tomilov-G <tomilovgleb591@gmail.com>
2025-07-22 17:34:39 +0700

    Добавлен шрифт

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/react-pizza/",
+  base: "/PizzaApp/",
   plugins: [react()],
   build: {
     sourcemap: true,
