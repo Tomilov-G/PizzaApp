bf712dd5015ad913ec3c19a0f3d68959c9a18ec6
Tomilov-G <tomilovgleb591@gmail.com>
2025-07-22 16:57:47 +0700

    Удаление лишних файлов

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
-  base: "/react-pizza/", 
+  base: "/react-pizza/",
   plugins: [react()],
   build: {
     sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          "cart-page": [path.resolve(__dirname, "src/pages/CartPage.tsx")],
-          "pizza-description-page": [
-            path.resolve(__dirname, "src/pages/PizzaDescriptionPage.tsx"),
-          ],
-          "not-found-page": [
-            path.resolve(__dirname, "src/pages/NotFoundPage.tsx"),
-          ],
-          vendor: ["react", "react-dom", "react-router-dom"],
-        },
-        chunkFileNames: "assets/[name].[hash].js",
-      },
-    },
   },
 });
